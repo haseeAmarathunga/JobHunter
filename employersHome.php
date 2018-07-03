4dea3f481b237e17ce88da2adbce9f30d771423e
Hasee Amarathunga <haseeamarathunga@gmail.com>
2018-07-02 21:28:08 -0700

    Update

---
+++ employersHome.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="style.css" >
+<head>
+	<title>JobHunter</title>
+</head>
+<body>
+	<div class="logo">
+	<a href="home.php"> <img src="image/logo.png"> </a></div>
+	
+	<div>
+	<center>
+	<h1><font color="2f3637">Welcome to</font> <font color="#ee2742">JobHunter</font></h1>
+	<br>
+	
+	</center>
+
+
+	</div>
+	
+	<!--
+	<a href="home.php"><img src="image/home.png"></a>
+	<a href="employers.php"><img src="image/employers.png"></a>
+	<a href="vacancy.php"><img src="image/vacancy.png"></a>
+	<a href="about.php"><img src="image/about.png"></a>
+	-->
+	<center>
+	<div class="btn-group">
+	<a href="employershome.php"><button class="button">Home</button></a>
+	<a href="employersProfile.php"><button class="button">Profile</button></a>
+	<a href="seekers.php"><button class="button">JobRequests</button></a>
+
+	<a href="index.php"><button class="button">Login</button></a>
+    </div>
+
+    </center>
+    <div class="container">
+		<h2 class="cat_title"><span id="redlight">Job</span> Categories</h2>
+		<hr class="cat_title"/>
+		<div class="catBox">
+			<ul>
+				<li>Banking/Insurance</li>
+				<li>Logistics/Warehouse/Transport</li>
+				<li>Hospital/Nursing/Healthcare</li>
+				<li>HR/Training</li>
+				<li>Supervision/Quality Control</li>
+
+			</ul>
+			<ul>
+				<li>Teaching/Academic/Library</li>
+				<li>Accounting/Auditing/Finance</li>
+				<li>Civil Eng/Interior Design/Architecture</li>
+				<li>Eng-Mech/Auto/Elec</li>
+				<li>Programmers/Designers</li>
+
+			</ul>
+			<ul>
+				<li>Security</li>
+				<li>Legal/Law</li>
+				<li>Media/Advert/Communication</li>
+				<li>Hospital/Nursing/Healthcare</li>
+				<li>Technicians/Mechanics</li>
+
+			</ul>
+        </div>
+        <a href="employersReg.php"><button class="button2"><span>Post a Job</span></button></a>
+
+	</div>
+	
+	
+	
+	
+</body>
+</html>
