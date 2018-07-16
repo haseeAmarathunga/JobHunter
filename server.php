5d0341d7b9233345b8f1b7caf2d151c66a5368db
Hasee Amarathunga <haseeamarathunga@gmail.com>
2018-07-15 17:32:25 -0700

    update

--- server.php
+++ server.php
@@ -2,9 +2,7 @@
    //connecting database
    $db = mysqli_connect("localhost","root", "","jobhunter") or die("Error connecting to database");
    session_start();
-   
    $error = "";
-
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
       
@@ -12,25 +10,38 @@
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
       
-      $sql = "SELECT id FROM userdetails WHERE name = '$myusername' and password = '$mypassword'";
+      $sql = "SELECT * FROM jobseekers WHERE email='$myusername' and pass = '$mypassword'";
       $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      //$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       
-      
-      $count = mysqli_num_rows($result);
+      $message=$myusername;
+      $_SESSION["message"]=$message;
       
       // If result matched $myusername and $mypassword, table row must be 1 row
 	//userName = admin    password=clash
 	
-      if($count == 1) {
-         
-         $_SESSION['login_user'] = $myusername;
-         
-         header("location:home.php");
+      if(mysqli_num_rows($result) == 1) {
+            header("location:seekersHome.php");
+           
+
       }else {
-         $error = "Your Login Name or Password is invalid";
-		 $_SESSION["error"]=$error;
-		 header("location:index.php");
+         $sql = "SELECT * FROM jobemployers WHERE email='$myusername' and pass = '$mypassword'";
+         $result = mysqli_query($db,$sql);
+         while($row = $result->fetch_assoc()) 
+         {
+            $name=$row["firstName"];
+            $_SESSION["name"]=$name;
+         }
+
+         if(mysqli_num_rows($result) == 1) {
+            header("location:employersHome.php");
+         }
+         else{
+            $error = "Your Login Name or Password is invalid";
+            $_SESSION["error"]=$error;
+            header("location:index.php");
+         }
       }
    }
+   
 ?>
