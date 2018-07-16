385b0c7e0eca47fcde91fad4acfb6977ace709d9
Hasee Amarathunga <haseeamarathunga@gmail.com>
2018-07-16 16:40:00 -0700

    Update

---
+++ seekerSuccess.php
@@ -0,0 +1,51 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="style.css" >
+<head>
+	<title>JobHunter</title>
+</head>
+<body>
+	<div class="logo">
+	<a href="home.php"> <img src="image/logo.png"> </a></div>
+	
+	<div>
+	<center>
+	
+	<br>
+	
+	</center>
+
+
+	</div>
+	
+	<!--
+	<a href="home.php"><img src="image/home.png"></a>
+	<a href="employers.php"><img src="image/employers.png"></a>
+	<a href="vacancy.php"><img src="image/vacancy.png"></a>
+	<a href="about.php"><img src="image/about.png"></a>
+	-->
+	<center>
+
+
+    <h2><font color="2f3637">Congratulations! Your request POSTED</font> <font color="#ee2742"> Successfully.
+    </font>
+    <font color="2f3637"><br>They will inform you if you are selected.</font>
+    </h2>
+    
+	<a href="seekersHome.php"><button class="button2"><span>Back to Home</span></button></a>
+	
+	</center>
+	</div>
+	</center>
+	
+	
+	
+	
+	
+</body>
+</html>
---
+++ success.php
@@ -0,0 +1,48 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="style.css" >
+<head>
+	<title>JobHunter</title>
+</head>
+<body>
+	<div class="logo">
+	<a href="home.php"> <img src="image/logo.png"> </a></div>
+	
+	<div>
+	<center>
+	
+	<br>
+	
+	</center>
+
+
+	</div>
+	
+	<!--
+	<a href="home.php"><img src="image/home.png"></a>
+	<a href="employers.php"><img src="image/employers.png"></a>
+	<a href="vacancy.php"><img src="image/vacancy.png"></a>
+	<a href="about.php"><img src="image/about.png"></a>
+	-->
+	<center>
+
+
+    <h2><font color="2f3637">Congratulations! job post</font> <font color="#ee2742"> Successfully.</font></h2>
+    
+	<a href="employersHome.php"><button class="button2"><span>Back to Home</span></button></a>
+	
+	</center>
+	</div>
+	</center>
+	
+	
+	
+	
+	
+</body>
+</html>
